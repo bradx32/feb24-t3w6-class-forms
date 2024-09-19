c99a44443a86afc9fdcff7375f3c5c95ae2409a7
Bradley Richmond <bradx32@hotmail.com>
2024-09-19 20:41:54 +1000

    feat: example of many posts simultaneously

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="App">
       <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      
     </div>
   );
 }
